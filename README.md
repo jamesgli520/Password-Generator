e35e770890e27ffb0b4e8fcf3c580220fd2ccb45
James Li <jgdli0701@gmail.com>
2021-07-13 00:34:10 -0400

    Readme and js updated, locally

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Password-Generator
+
+**Version 1.0.0**
+
+## Description
+This is a password generator that generates the password randomly. </br></br>
+
+The application has several functionalities:</br>
+    1) Prompt window for user to input.</br>
+    2) Check the length of password which it has to be at least 8 characters
+       and no more than 128 characters.</br>
+    3) Users are able to confirm whether or not to include character types.</br>
+    4) Check the input that it has to be at least one character type in the
+       password.</br></br>
+       
+    
+There are four type of character in the password:</br>
+    1) Numeric 0-9</br>
+    2) 11 Special Characters "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "?"</br>
+    3) 26 Lowercase Alphabet</br>
+    4) 26 Uppercase Alphabet</br></br>
+
+## Screenshot
+<p>
+    <img src="image\homepage.PNG" />
+    <img src="image\length.PNG" />
+    <img src="image\num.PNG" />
+    <img src="image\sp.PNG" />
+    <img src="image\lc.PNG" />
+    <img src="image\uc.PNG" />
+    <img src="image\password.PNG" />
+</p>
+
+
+## Link to Deployed Application
+
+Live: 
+
+Github repository: 
+
+## License & copyright
+
+**Made By James** 
+
+&copy; 2021.
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ var numGenerated;
 var scGenerated;
 var lcGenerated;
 var ucGenerated;
+var validationResult;
 
 function generatePassword(){
     var passwordKeeper;
@@ -32,11 +33,11 @@ function generatePassword(){
                 }
             }
 
-            passNumberMethod();
-            passSpecialrMethod();
-            passLowercaseMethod();
-            passUppercaseMethod();
-            validation();
+            numGenerated = passNumberMethod();
+            scGenerated = passSpecialrMethod();
+            lcGenerated = passLowercaseMethod();
+            ucGenerated = passUppercaseMethod();
+            return validationResult = validation();
             
         }else{
             if(confirm("The length of password is necessary, plase start over")){
@@ -46,202 +47,197 @@ function generatePassword(){
             }
         } 
     }
-
-    function passNumberMethod(){
-        //Generate Numbers
-        var passNumber = confirm("Would you like your password includes number?");
-        if(passNumber){
-            var numGenerated = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.random() *10;
-                var randomNumber = Math.floor(index);
-                numGenerated[i] = randomNumber;
-            }
-            
-        }
-        return numGenerated;
-    }
-
-    function passSpecialrMethod(){
-        //Generate Special Characters
-        var passSpecial = confirm("Would you like your password includes special Characters?");
-        if(passSpecial){
-            var specialCharacter = ["!","@","#","$","%","^","&","*","(",")","?"];
-            var scGenerated = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * 11);
-                scGenerated[i] = specialCharacter[index];
-            }
-        }
-        return scGenerated;
-    }
-
-    function passLowercaseMethod(){
-        //Generate Lowercase Letters
-        var passLowercase = confirm("Would you like your password includes lowercase letters?");
-        if(passLowercase){
-            var text = "abcdefghjiklmnopqrstuvwxyz";
-            var lowercaseLetter = text.split("");
-            var lcGenerated = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * 26);
-                lcGenerated[i] = lowercaseLetter[index];
-            }
-        }
-        return lcGenerated;
-    }
-    function passUppercaseMethod(){
-        //Generate Uppercase Letters
-        var passUppercase = confirm("Would you like your password includes uppercase letters?");
-        if(passUppercase){
-            var text = "abcdefghjiklmnopqrstuvwxyz";
-            var uppercaseLetter = text.toUpperCase().split("");
-            var ucGenerated = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * 26);
-                ucGenerated[i] = uppercaseLetter[index];
-            }
-        }
-        return ucGenerated;
-    }
-
-    function validation(){
-        //validation input that check at least one character type should be selected
-        console.log("num"+numGenerated);
-        console.log("sp"+scGenerated);
-        console.log("lc"+lcGenerated);
-        console.log("uc"+ucGenerated);
-
-
-        if(numGenerated == null && scGenerated == null && lcGenerated == null && ucGenerated == null){
-            alert("At least one character type should be selected");
-            if(confirm("Would you like to start over")){
-                lengthOfPassword();
-            }else{
-                return;
-            }
-        }else if(numGenerated == null){
-            console.log(numGenerated);
-            var preGenerated = scGenerated +","+ lcGenerated +","+ ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split(""); 
-            var actualPassword;
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            
-            return actualPassword;
-        }else if(scGenerated == null){
-            var preGenerated = numGenerated +","+ lcGenerated +","+ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }else if(lcGenerated == null){
-            var preGenerated = scGenerated +","+ numGenerated +","+ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;           
-        }else if(ucGenerated == null){
-            var preGenerated = scGenerated +","+ lcGenerated +","+numGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;           
-        }
-        else if(numGenerated == null && scGenerated == null){
-            var preGenerated = lcGenerated +","+ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }else if(numGenerated == null && lcGenerated == null){
-            var preGenerated = scGenerated +","+ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }else if(numGenerated == null && ucGenerated == null){
-            var preGenerated = scGenerated +","+lcGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }
-        else if(numGenerated == null && scGenerated == null && lcGenerated == null){
-            var preGenerated = ucGenerated;
-            var removeComma;
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * preGenerated.length);
-                actualPassword[i] = preGenerated[index];
-            }
-            removeComma = actualPassword.toString().replaceAll(",","");
-            return removeComma;
-        }else if(numGenerated == null && scGenerated == null && ucGenerated == null){
-            var preGenerated = lcGenerated;
-            var removeComma = preGenerated.toString().replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }else if(numGenerated == null && lcGenerated == null && ucGenerated == null){
-            var preGenerated = scGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            return actualPassword;
-        }else {
-            var preGenerated = numGenerated +","+ scGenerated +","+ lcGenerated +","+ucGenerated;
-            var removeComma = preGenerated.replaceAll(",","");
-            var postGenerated = removeComma.split("");
-            var actualPassword = new Array();
-            for(var i = 0; i < passLength; i++){
-                var index = Math.floor(Math.random() * postGenerated.length);
-                actualPassword = actualPassword + postGenerated[index];
-            }
-            console.log("hello");
-            return actualPassword;
-        }
-    }
-   
-    
     //return password
     passwordKeeper = lengthOfPassword();
     return passwordKeeper;
+}
+
+
+function passNumberMethod(){
+    //Generate Numbers
+    var passNumber = confirm("Would you like your password includes number?");
+    var numGenerated;
+    if(passNumber){
+        numGenerated = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.random() *10;
+            var randomNumber = Math.floor(index);
+            numGenerated[i] = randomNumber;
+        }
+        
+    }
+    return numGenerated;
+}
+
+function passSpecialrMethod(){
+    //Generate Special Characters
+    var passSpecial = confirm("Would you like your password includes special Characters?");
+    var scGenerated;
+    if(passSpecial){
+        var specialCharacter = ["!","@","#","$","%","^","&","*","(",")","?"];
+        scGenerated = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * 11);
+            scGenerated[i] = specialCharacter[index];
+        }
+    }
+    return scGenerated;
+}
+
+function passLowercaseMethod(){
+    //Generate Lowercase Letters
+    var passLowercase = confirm("Would you like your password includes lowercase letters?");
+    var lcGenerated;
+    if(passLowercase){
+        var text = "abcdefghjiklmnopqrstuvwxyz";
+        var lowercaseLetter = text.split("");
+        lcGenerated = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * 26);
+            lcGenerated[i] = lowercaseLetter[index];
+        }
+    }
+    return lcGenerated;
+}
+function passUppercaseMethod(){
+    //Generate Uppercase Letters
+    var passUppercase = confirm("Would you like your password includes uppercase letters?");
+    var ucGenerated;
+    if(passUppercase){
+        var text = "abcdefghjiklmnopqrstuvwxyz";
+        var uppercaseLetter = text.toUpperCase().split("");
+        ucGenerated = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * 26);
+            ucGenerated[i] = uppercaseLetter[index];
+        }
+    }
+    return ucGenerated;
+}
+
+function validation(){
+    //validation input that check at least one character type should be selected
+    if(numGenerated == null && scGenerated == null && lcGenerated == null && ucGenerated == null){
+        alert("At least one character type should be selected");
+        if(confirm("Would you like to start over")){
+            lengthOfPassword();
+        }else{
+            return;
+        }
+    }else if(numGenerated === null){
+        var preGenerated = scGenerated +","+ lcGenerated +","+ ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split(""); 
+        var actualPassword;
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+            console.log(numGenerated);
+        }
+        return actualPassword;
+    }else if(scGenerated === null){
+        var preGenerated = numGenerated +","+ lcGenerated +","+ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }else if(lcGenerated === null){
+        var preGenerated = scGenerated +","+ numGenerated +","+ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;           
+    }else if(ucGenerated === null){
+        var preGenerated = scGenerated +","+ lcGenerated +","+numGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;           
+    }
+    else if(numGenerated === null && scGenerated === null){
+        var preGenerated = lcGenerated +","+ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }else if(numGenerated === null && lcGenerated === null){
+        var preGenerated = scGenerated +","+ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }else if(numGenerated === null && ucGenerated === null){
+        var preGenerated = scGenerated +","+lcGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }
+    else if(numGenerated === null && scGenerated === null && lcGenerated === null){
+        var preGenerated = ucGenerated;
+        var removeComma;
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * preGenerated.length);
+            actualPassword[i] = preGenerated[index];
+        }
+        removeComma = actualPassword.toString().replaceAll(",","");
+        return removeComma;
+    }else if(numGenerated === null && scGenerated === null && ucGenerated === null){
+        var preGenerated = lcGenerated;
+        var removeComma = preGenerated.toString().replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }else if(numGenerated === null && lcGenerated === null && ucGenerated === null){
+        var preGenerated = scGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }else {
+        var preGenerated = numGenerated +","+ scGenerated +","+ lcGenerated +","+ucGenerated;
+        var removeComma = preGenerated.replaceAll(",","");
+        var postGenerated = removeComma.split("");
+        var actualPassword = new Array();
+        for(var i = 0; i < passLength; i++){
+            var index = Math.floor(Math.random() * postGenerated.length);
+            actualPassword = actualPassword + postGenerated[index];
+        }
+        return actualPassword;
+    }
 }
 
 
